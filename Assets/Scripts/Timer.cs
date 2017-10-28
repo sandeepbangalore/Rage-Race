87522c57a295184f14329da424a0240ff118d04e
sandeepbangalore <sandeepbangalor@gmail.com>
2017-10-28 17:58:28 -0400

    Track alpha

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer {
+
+	private float timer = 0.0f;
+	public Timer () {
+		timer = 0.0f;
+	}
+
+	public void Tick (float tick) {
+		if (timer > 0.0f)
+			timer -= tick;
+		else
+			timer = 0.0f;
+	}
+	public void AddTime (float addTime) {
+		timer += addTime;
+	}
+	public float GetTime (){
+		return timer;
+	}
+	public void Reset (){
+		timer = 0.0f;
+	}
+}
