1aab8e3104cdcb2b2629dfd6bcf095e5c65116f4
tina <christinachung2014@gmail.com>
2017-11-12 21:52:40 -0500

    Added credits to new scripts

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/PowerupScript.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/PowerupScript.cs
@@ -1,4 +1,16 @@
-﻿using System.Collections;
+﻿
+/*
+ *                                  Team Rocket 
+ * 
+ *  Agneya A Kerure                 akerure3        kerure.agneya@gatech.edu
+ *  Christina Chung                 cchung44        cchung44@gatech.edu
+ *  Erin Hsu                        ehsu7           ehsu7@gatech.edu
+ *  Dibyendu Mondal                 dmondal6        dibyendu@gatech.edu
+ *  Sandeep Banaglore Venkatesh     sbv7            sandeepbanaglore@gatech.edu
+ * 
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
