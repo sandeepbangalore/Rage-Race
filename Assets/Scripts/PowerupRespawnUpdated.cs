04f72003bf2dfebacabfa7af039efbe293445c32
sandeepbangalore <sandeepbangalor@gmail.com>
2017-12-07 16:44:57 -0500

    respawn weighted probabilities

--- Assets/Scripts/PowerupRespawnUpdated.cs
+++ Assets/Scripts/PowerupRespawnUpdated.cs
@@ -13,6 +13,7 @@ using UnityEngine;
 public class PowerupRespawnUpdated : MonoBehaviour
 {
 	public GameObject[] pickup;
+	public float[] pickUpProbability;
 	public float spawnTime = 5f;
 	public Transform[] spawnPoints;
 	int layerMask;
@@ -20,8 +21,12 @@ public class PowerupRespawnUpdated : MonoBehaviour
 	{
 		// Get the layerMask for pick-up objects
 		layerMask = LayerMask.GetMask ("Pickup");
-		Debug.Log (layerMask);
+		//Debug.Log (layerMask);
 		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		if (pickup.Length != pickUpProbability.Length) {
+			Debug.Log ("All Probabilities not specified");
+		}
+			
 	}
 	void Spawn ()
 	{
@@ -31,11 +36,24 @@ public class PowerupRespawnUpdated : MonoBehaviour
 			Collider[] hitCollider = Physics.OverlapSphere (spawnPoints [spawnPointIndex].position, 2f);
 			// Instantiate only if no other exists in the location
 			if (hitCollider.Length == 0) {
-				GameObject temp = Instantiate (pickup[Random.Range(0, pickup.Length)], spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation) as GameObject;
+				GameObject temp = Instantiate (pickup[getIndex()], spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation) as GameObject;
 			}
 			else{
 				Debug.Log (hitCollider [0]);
 			}
 		}
+	}
+
+	int getIndex(){
+		float prob = Random.value;
+		for (int i= 0; i < pickUpProbability.Length; i++) {
+			if (prob < pickUpProbability[i]) {
+				return i;
+			}
+			else {
+				prob -= pickUpProbability[i];
+			}
+		}
+		return pickUpProbability.Length - 1;
 	}
 }
